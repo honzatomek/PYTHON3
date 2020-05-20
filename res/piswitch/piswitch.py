399628714e2699f977c6d84f76b03fac6b11e13f
honzatomek <rpi3.tomek@protonmail.com>
2020-05-21 00:06:07 +0200

    piswitch v0.0.1

--- res/piswitch/piswitch.py
+++ res/piswitch/piswitch.py
@@ -1,14 +1,30 @@
-#!/usr/local/bin/python3.7
+#!/usr/bin/python3.5
 
-# Import the libraries to use time delays, send os commands and access GPIO pins
-import RPi.GPIO as GPIO
-import time
-import os
+# Import the modules to send commands to the system and access GPIO pins
+from subprocess import call
+import RPi.GPIO as gpio
+import datetime
+
+# Define a function to keep script running
+def loop():
+    input()
+
+def now():
+    '''
+    Returns current date and time in YYYYMMDD-HH:MM:SS format
+    '''
+    return '{0:%Y%m%d-%H:%M:%S}'.format(datetime.datetime.now())
  
-GPIO.setmode(GPIO.BOARD) # Set pin numbering to board numbering
-GPIO.setup(7, GPIO.IN) # Setup pin 7 as an input
-while True: # Setup a while loop to wait for a button press
-    if(GPIO.input(7)): # Setup an if loop to run a shutdown command when button press sensed
-        os.system("sudo poweroff") # Send shutdown command to os
-        break
-    time.sleep(1) # Allow a sleep time of 1 second to reduce CPU usage
+# Define a function to run when an interrupt is called
+def shutdown(pin):
+    call('poweroff', shell=False)
+ 
+with open('/home/pi/log/piswitch.log', 'a', encoding='utf-8') as log:
+	log.write(now() + '\n')
+gpio.setmode(gpio.BOARD) # Set pin numbering to board numbering
+gpio.setup(7, gpio.IN) # Set up pin 7 as an input
+gpio.add_event_detect(7, gpio.RISING, callback=shutdown, bouncetime=200) # Set up an interrupt to look for button presses
+ 
+loop() # Run the loop function to keep script running
+
+
